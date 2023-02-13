f59ea3b5956529754e8902d8c79aee940dc3150e
Chris Pressey <cpressey@catseye.tc>
2023-02-13 08:49:43 +0000

    Remove `where`s and most forward references.

--- src/Language/Lanthorn/LetRec2.hs
+++ src/Language/Lanthorn/LetRec2.hs
@@ -4,59 +4,63 @@ import Language.Lanthorn.AST
 
 
 convert ast =
-    case ast of
-        (Fun formals body) -> Fun formals (convert body)
-        (Apply name args) -> Apply name (map (convert) args)
-        (LetRec bindings body) ->
+    let
+        wrapperNameOuter name = name ++ "$0"
+        wrapperNameInner name = name ++ "$1"
+        createLocalBindings injecteds allInjectedNames =
             let
-                bindings' = map (\(name, expr) -> (name, (convert expr))) bindings
-                body' = convert body
-                injecteds = map (\(name, (Fun formals _)) -> (name, formals)) bindings'
-                enrichedBindings = createEnrichedBindings bindings' injecteds
-                wrapperBindings = createWrapperBindings bindings' injecteds
-            in
-                LetStar (enrichedBindings ++ wrapperBindings) body'
-        (If c t f) -> If (convert c) (convert t) (convert f)
-        (LetStar bindings body) -> LetStar (map (\(name, expr) -> (name, (convert expr))) bindings) (convert body)
-        other -> other
-
-wrapperNameOuter name = name ++ "$0"
-wrapperNameInner name = name ++ "$1"
-
-createEnrichedBindings bindings injecteds =
-    map createEnrichedBinding bindings where
-        createEnrichedBinding binding =
-            case binding of
-                (name, (Fun formals body)) ->
+                createLocalBinding (injectedName, formals) =
                     let
-                        name' = wrapperNameOuter name
-                        injectedNames = map (fst) injecteds
-                        formals' = formals ++ (map (wrapperNameInner) injectedNames)
-                        body' = (LetStar (createLocalBindings injecteds injectedNames) body)
-                        expr' = (Fun formals' body')
+                        formals' = map (wrapperNameInner) formals
+                        actuals = map (ValueOf) (formals' ++ (map (wrapperNameInner) allInjectedNames))
                     in
-                        (name', expr')
-                other ->
-                    other
-
-createLocalBindings injecteds allInjectedNames =
-    map createLocalBinding injecteds where
-        createLocalBinding (injectedName, formals) =
+                        (injectedName, Fun formals' (Apply (wrapperNameInner injectedName) actuals))
+            in
+                map createLocalBinding injecteds
+        createEnrichedBindings bindings injecteds =
             let
-                formals' = map (wrapperNameInner) formals
-                actuals = map (ValueOf) (formals' ++ (map (wrapperNameInner) allInjectedNames))
+                createEnrichedBinding binding =
+                    case binding of
+                        (name, (Fun formals body)) ->
+                            let
+                                name' = wrapperNameOuter name
+                                injectedNames = map (fst) injecteds
+                                formals' = formals ++ (map (wrapperNameInner) injectedNames)
+                                body' = (LetStar (createLocalBindings injecteds injectedNames) body)
+                                expr' = (Fun formals' body')
+                            in
+                                (name', expr')
+                        other ->
+                            other
             in
-                (injectedName, Fun formals' (Apply (wrapperNameInner injectedName) actuals))
-
-createWrapperBindings bindings injecteds =
-    foldl createWrapperBinding [] bindings where
-        createWrapperBinding acc binding =
-            case binding of
-                (name, (Fun formals body)) ->
-                    let
-                        actuals = map (ValueOf) (formals ++ (map (\x -> wrapperNameOuter $ fst x) injecteds))
-                        expr' = Fun formals (Apply (wrapperNameOuter name) actuals)
-                    in
-                        acc ++ [(name, expr')]
-                other ->
-                    acc
+                map createEnrichedBinding bindings
+        createWrapperBindings bindings injecteds =
+            let
+                createWrapperBinding acc binding =
+                    case binding of
+                        (name, (Fun formals body)) ->
+                            let
+                                actuals = map (ValueOf) (formals ++ (map (\x -> wrapperNameOuter $ fst x) injecteds))
+                                expr' = Fun formals (Apply (wrapperNameOuter name) actuals)
+                            in
+                                acc ++ [(name, expr')]
+                        other ->
+                            acc
+            in
+                foldl createWrapperBinding [] bindings
+    in
+        case ast of
+            (Fun formals body) -> Fun formals (convert body)
+            (Apply name args) -> Apply name (map (convert) args)
+            (LetRec bindings body) ->
+                let
+                    bindings' = map (\(name, expr) -> (name, (convert expr))) bindings
+                    body' = convert body
+                    injecteds = map (\(name, (Fun formals _)) -> (name, formals)) bindings'
+                    enrichedBindings = createEnrichedBindings bindings' injecteds
+                    wrapperBindings = createWrapperBindings bindings' injecteds
+                in
+                    LetStar (enrichedBindings ++ wrapperBindings) body'
+            (If c t f) -> If (convert c) (convert t) (convert f)
+            (LetStar bindings body) -> LetStar (map (\(name, expr) -> (name, (convert expr))) bindings) (convert body)
+            other -> other
